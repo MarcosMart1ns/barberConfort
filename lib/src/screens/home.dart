20a9185820f0069acbed73e52bc5ca36a31191b5
Marcos Martins <marcos_36ma@hotmail.com>
2020-10-26 13:56:02 -0300

    ..

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -16,6 +16,46 @@ class HomeState extends State {
           showLogo(),
           Text('Faça seu login',
               style: Theme.of(context).primaryTextTheme.headline6),
+          Expanded(
+              child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            child: Form(
+                //key:,
+                child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value.trim().isEmpty) {
+                      return 'Campo não pode estar vazio';
+                    }
+                    return null;
+                  },
+                  //controller:,
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.trim().isEmpty) {
+                      return 'Campo não pode estar vazio,insira uma senha';
+                    }
+                    return null;
+                  },
+                  //controller:,
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                )
+              ],
+            )),
+          )),
         ],
       ),
     );
--- lib/src/themes/theme.dart
+++ lib/src/themes/theme.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class CustomTheme {
   ThemeData theme = ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Colors.grey[900],
-      backgroundColor: Colors.grey[900],
-      primaryTextTheme: TextTheme(
-        headline6: TextStyle(color: Colors.white),
-      ),
-      fontFamily: 'Roboto Slab');
+    brightness: Brightness.light,
+    primaryColor: Colors.grey[900],
+    backgroundColor: Colors.grey[900],
+    primaryTextTheme: TextTheme(
+      headline6: TextStyle(color: Colors.white, fontSize: 20),
+      bodyText1: TextStyle(color: Colors.white),
+    ),
+    fontFamily: 'Roboto Slab',
+  );
 }
